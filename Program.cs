e833f5345e394b01ee9db3214e0b8be107a0d959
Sashikumar <yshashi30@gmail.com>
2024-02-19 00:25:00 +0800

    feat:changed API sashi

--- Program.cs
+++ Program.cs
@@ -21,18 +21,7 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
+app.MapGet("/sashi", () =>"Welcome sashi")
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
