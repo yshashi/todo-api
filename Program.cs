b85f4dc2b1d9e1e12a4f699a0e82510ac223b03c
Sashikumar <yshashi30@gmail.com>
2024-02-19 00:40:46 +0800

    refactor: reverted the changes

--- Program.cs
+++ Program.cs
@@ -21,7 +21,18 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/sashi", () =>"Welcome sashi")
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast =  Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
